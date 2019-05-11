b9aba2703e19f448478a24626dae11523d474f8f
David Harrigan <dharrigan@gmail.com>
2019-05-11 12:25:10 +0100

    Add in main method

--- src/palindrome/core.clj
+++ src/palindrome/core.clj
@@ -1,7 +1,10 @@
-(ns palindrome.core
-  (:require [clojure.string :as str]))
+(ns palindrome.core (:require [clojure.string :as str]))
 
 (defn palindrome?
   [palindrome]
   (let [y (str/lower-case (str/join (re-seq #"[A-Za-z]" palindrome)))]
     (= (str/reverse y) y)))
+
+(defn -main
+  [& args]
+  (palindrome? (first args)))
