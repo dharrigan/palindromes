5dc2f5a8ef5c0eb8a644c3979a2a88e9f0302667
David Harrigan <dharrigan@gmail.com>
2019-05-20 09:17:36 +0100

    Updates

---
+++ test/palindrome/core_test.clj
@@ -0,0 +1,40 @@
+(ns palindrome.core-test
+  (:require [clojure.test :as t]
+            [clojure.test.check.generators :as gen]
+            [clojure.string :as str]
+            [palindrome.core :as p]))
+
+(def palindromes [
+                  "Able was I ere I saw Elba"
+                  "A man, a plan, a canal – Panama"
+                  "Madam, I'm Adam"
+                  "Never odd or even"
+                  "Doc, note: I dissent. A fast never prevents a fatness. I diet on cod"
+                  "redivider"
+                  "deified"
+                  "civic"
+                  "radar"
+                  "level"
+                  "rotor"
+                  "kayak"
+                  "reviver"
+                  "racecar"
+                  "redder"
+                  "madam"
+                  "refer"])
+
+(def gen-strings
+  (gen/sample (gen/fmap #(apply str %) (gen/vector gen/char-alpha 10)) 100))
+
+(defn gen-palindromes
+  []
+  (for [string gen-strings]
+    (str/reverse (str string (str/reverse string)))))
+
+(t/deftest generated-palindromes
+  (doseq [palindrome (gen-palindromes)]
+    (t/is #(= true %) (p/palindrome? palindrome))))
+
+(t/deftest english-palindromes
+  (doseq [palindrome palindromes]
+    (t/is #(= true %) (p/palindrome? palindrome))))
